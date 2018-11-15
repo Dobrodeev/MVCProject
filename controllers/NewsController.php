0f19b031abe0390643f1dce6fb2e498962fcf6a7
dobrodeev <dobrod@meta.ua>
2018-11-16 00:46:24 +0200

    make parametrs for DÈ (login, password etc.)

---
+++ controllers/NewsController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dobrodeev
+ * Date: 08.11.2018
+ * Time: 17:57
+ */
+include_once (ROOT.'/models/News.php');
+class NewsController
+{
+    public function actionIndex()
+    {
+        $newslist = array();
+    }
+
+    public function actionNew()
+    {
+
+    }
+}
---
+++ models/News.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dobrodeev
+ * Date: 15.11.2018
+ * Time: 16:36
+ */
+class News
+{
+    public static function getNewsList()
+    {
+        $db =
+    }
+}
