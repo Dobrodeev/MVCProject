379b552f930e559de79ef1bc00b7204245c27f0b
dobrodeev <dobrod@meta.ua>
2018-11-18 18:03:13 +0200

    make query to DB and get all news than write tham to array $newslist

--- models/News.php
+++ models/News.php
@@ -9,6 +9,21 @@ class News
 {
     public static function getNewsList()
     {
-        $db =
+        $db = DB::getConnect();// подключение к БД
+        $newlist = array();
+        $result = $db->query('SELECT id, title, date, author_name, short_content FROM news');
+        $i = 0;
+        while ($row = $result->fetch())
+        {
+            $newlist[$i]['id'] = $row['id'];
+            $newlist[$i]['title'] = $row['title'];
+            $newlist[$i]['date'] = $row['dte'];
+            $newlist[$i]['author_name'] = $row['author_name'];
+            $newlist[$i]['short_content'] = $row['short_content'];
+        }
+        return $newlist;
+//        echo '<pre>';
+//        print_r($newlist);
+//        echo '</pre>';
     }
 }
