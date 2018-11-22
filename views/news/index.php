3a6affd91b01059921e4d21d2ccded70d2100aa7
dobrodeev <dobrod@meta.ua>
2018-11-22 22:45:45 +0200

    убрали отладочные строки

--- views/news/index.php
+++ views/news/index.php
@@ -1,12 +1,13 @@
 <?php
+/**
 foreach ($newslist as $value)
 {
+    echo '<pre>';
     echo $value['preview'];
+    echo '</pre>';
 }
-echo '<pre>';
-        print_r($newslist['author_name']);
-        echo '</pre>';
-        ?>
+ */
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
