02a1a7367e328a8071e4d100334cbd106a32baec
Dobrodeev <dobrod@meta.ua>
2019-01-07 22:24:50 +0200

    убрали отладочные строки

--- index.php
+++ index.php
@@ -9,8 +9,6 @@ define('ROOT', dirname(__FILE__));
 require_once (ROOT.'/components/Router.php');
 require_once (ROOT.'/components/DB.php');
 
-//echo ROOT;
-
 $router = new Router();
 $router->run();
-//print_r();
+
